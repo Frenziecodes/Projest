8e6ff067843f8beba99936632b9a3f8ed9edbfba
Lewis ushindi <coderflame3@gmail.com>
2023-01-26 15:50:14 +0300

    validating registration-form on client side

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,5 +9,46 @@ var dropdown = document.querySelectorAll('.dropdown');
     });
   }
 
-  // Form validation
+  // Registration form validation
+
+  function validateRegistration(){
+    var username = document.register.name.value;
+    var userEmail = document.register.email.value;
+    var userPass = document.register.pass.value;
+    var userPass2 = document.register.pass2.value;
+
+    // validating username
+    if(username == ''){
+      alert("Username is Required")
+      return false;
+    }
+    if(username.length<4){
+      alert('username should be at least 4 character')
+    }
+
+    // validating email
+    at = userEmail.indexOf('@');
+    dot = userEmail.lastIndexOf('.');
+    if (userEmail == '') {
+      alert('Email is required')
+    }
+    if (at <1 || (dot - at < 2)) {
+      alert('Incorrect Email')
+    }
+
+    // validating password
+    if(userPass == ''){
+      alert('Password is required')
+    }
+    if (userPass.length < 6) {
+      alert('Password should be at least 6 characters')
+    }
+
+    // validating repeat password
+    if (userPass2 != userPass) {
+      alert('Passwords do not match')
+    }
+
+  }
+  // end of registration form validation
 
