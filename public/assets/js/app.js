a1e66a4105bb8a4cd0af56b465afa461f7ffeb66
Lewis ushindi <coderflame3@gmail.com>
2023-01-30 18:32:17 +0300

    adding side nav

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -70,3 +70,13 @@ var dropdown = document.querySelectorAll('.dropdown');
   }
   // end of registration form validation
 
+
+  /* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
