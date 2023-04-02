9b0e0799d1b9903f47b36020f525154c27639c3c
Kalp <kalp2002prajapati@gmail.com>
2023-04-02 17:09:42 +0530

    searching functionality added in view project page

--- src/pages/ViewProject.jsx
+++ src/pages/ViewProject.jsx
@@ -3,20 +3,52 @@ import React, { useEffect, useState } from 'react'
 import { db } from '../firebase'
 
 function ViewProject() {
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
+  const [userResult, setUserResult] = useState([]);
   const [loader, setLoader] = useState(true)
   const userData = collection(db, 'projects')
+  const [searchVal, setSearchVal] = useState("");
+
+  useEffect(() => {
+    if (searchVal === "") {
+      return setUserResult(users);
+    }
+
+    const filteredData = users.filter((item) => {
+
+      if (item.title.toLowerCase().includes(searchVal.toLowerCase())) {
+        return true;
+      }
+
+      if (item.description.toLowerCase().includes(searchVal.toLowerCase())) {
+        return true;
+      }
+
+      const list = item.tags.filter((it) => {
+        if (it.toLowerCase().includes(searchVal.toLowerCase())) {
+          return true;
+        }
+        return false
+      });
+
+      return list.length > 0;
+
+    });
+
+    return setUserResult(filteredData);
+
+  }, [searchVal])
 
   useEffect(() => {
     const getData = async () => {
-      const data = await getDocs(userData)
-      setUsers(data.docs.map(doc => ({ ...doc.data(), id: doc.id })))
+      const data = await getDocs(userData);
+      const result = data.docs.map(doc => ({ ...doc.data(), id: doc.id }));
+      setUsers(result)
+      setUserResult(result)
       setLoader(false)
     }
     getData()
   }, [])
-
-  // console.log(users)
 
   return (
     loader ? (
@@ -27,10 +59,13 @@ function ViewProject() {
       </div>
     ) : (
       <div className='h-[100%] p-[20px]'>
+        <div className='flex justify-center'>
+          <input type="text" placeholder='Search projects here' className=' placeholder:text-slate-500 block bg-white w-[90vw] md:w-[36vw] lg:w-[32vw] border border-slate-300 rounded-md my-4 py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1' onChange={e => setSearchVal(e.target.value)} ></input>
+          {/* <BsSearch onClick={handleSearchClick} /> */}
+        </div>
         <div className='grid sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-[40px] px-[20px]'>
           {
-            users.map(user => {
-              {/* console.log(user) */ }
+            userResult.map(user => {
               return (
                 <div key={user.id} className="flex flex-col text-black items-center p-[20px] h-[100%] bg-blue-100 rounded-sm hover:-translate-y-1 hover:scale-110 hover:bg-blue-200 duration-300">
                   <h2 className='text-3xl text-blue-600 font-[700] mb-[10px]'>{user.title}</h2>
@@ -40,10 +75,10 @@ function ViewProject() {
                     <a href={user.projectGithubLink} className=" h-[100%] ml-[20px] px-[10px] py-[10px] bg-blue-500 text-white rounded-sm font-[500] " target="_blank">Project Link</a>
                   </div>
                   <div>
-                  Project Tags : 
+                    Project Tags :
                     {
                       user.tags.map(tag => {
-                        return(<span className=''> {tag} ,</span>)
+                        return (<span className=''> {tag} ,</span>)
                       })
                     }
                   </div>
