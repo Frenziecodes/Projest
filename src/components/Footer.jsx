644ca23393ae48774deb529377778f291ed83bf7
Pushkaraj Kulkarni <pushkarajkulkarni07@gmail.com>
2023-04-11 16:35:43 +0530

    Fix the position of Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer className="bg-blue-600 text-center lg:text-left relative bottom-0 right-0 left-0">
+    <footer className="bg-blue-600 text-center lg:text-left mt-auto">
     <div className="text-white text-center p-4">
       © {new Date().getFullYear() } All rights reserved    
     </div>
