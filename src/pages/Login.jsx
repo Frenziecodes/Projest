1e2271cdf207a0b63061518ee871c479c4eefdf6
rakeshbabu23 <baburakesh2308@gmail.com>
2023-04-24 17:20:41 +0530

    Changes made in login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -23,45 +23,49 @@ const Login = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-    <h2 className="text-3xl font-bold mb-4">Login</h2>
-    {error && <p className="text-red-500 mb-4">{error}</p>}
-    <form onSubmit={handleLogin} className="flex flex-col space-y-4">
-      <div className="flex flex-col">
-        <label htmlFor="email" className="font-semibold mb-2">
-          Email:
-        </label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-          className="border-gray-300 bg-slate-300 rounded-lg px-3 py-2"
-        />
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <div className="w-full max-w-md bg-white rounded-lg border border-gray-300 px-4 py-6">
+        <h2 className="text-3xl font-bold mb-4 text-center">Login</h2>
+        {error && <p className="text-red-500 mb-4">{error}</p>}
+        <form onSubmit={handleLogin} className="flex flex-col space-y-4">
+          <div className="flex flex-col">
+            <label htmlFor="email" className="font-semibold mb-2">
+              Email:
+            </label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              className="border-gray-300 bg-gray-100 rounded-lg px-3 py-2"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="password" className="font-semibold mb-2">
+              Password:
+            </label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              className="border-gray-300 bg-gray-100 rounded-lg px-3 py-2"
+            />
+          </div>
+          <button type="submit" className="bg-blue-500 text-white font-semibold py-2 rounded-lg hover:bg-blue-600 transition-colors duration-300">
+            Login
+          </button>
+        </form>
+        <p className="mt-4 text-center">
+          Don't have an account?{" "}
+          <Link to="/register" className="text-blue-500 hover:text-blue-600 transition-colors duration-300">
+            Register here
+          </Link>
+          .
+        </p>
       </div>
-      <div className="flex flex-col">
-        <label htmlFor="password" className="font-semibold mb-2">
-          Password:
-        </label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-          className="border-gray-300 bg-slate-300 rounded-lg px-3 py-2"
-        />
-      </div>
-      <button type="submit" className="bg-blue-500 text-white font-semibold py-2 rounded-lg hover:bg-blue-600 transition-colors duration-300">
-        Login
-      </button>
-    </form>
-    <p className="mt-4">
-      Don't have an account? <Link to="/register" className="text-blue-500 hover:text-blue-600 transition-colors duration-300">Register here</Link>.
-    </p>
-  </div>
+    </div>
   );
 };
-
-export default Login;
