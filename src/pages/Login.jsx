8a615de08f000088a5be48ff8216ca87e4c4fdbe
rakeshbabu23 <baburakesh2308@gmail.com>
2023-04-24 17:33:56 +0530

    Changes made in login page and registration page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -69,3 +69,4 @@ const Login = () => {
     </div>
   );
 };
+export default Login;
--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 const Registration = () => {
