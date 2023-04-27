cf628bdc3a7bd9274e63f63bf6b2f7a5fddda2f4
Lewis ushindi <coderflame3@gmail.com>
2023-04-27 13:36:02 +0300

    updating layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,9 @@
-import Navbar from './Navbar';
-import Footer from './Footer';
 
 function Layout({ children }) {
     return (
         <div className="min-h-screen">
             <div className="flex flex-col min-h-screen">
-                {/* <Navbar /> */}
                 <main className="flex-1 overflow-y-auto">{children}</main>
-                {/* <Footer /> */}
             </div>
         </div>
 
