cdd277a9b952a96d0ee6f491d233313a88314b1f
Prabhjot <777prabhjot@gmail.com>
2023-04-28 18:02:54 +0530

    made enhancment for skills section

--- src/pages/AddProject.jsx
+++ src/pages/AddProject.jsx
@@ -4,24 +4,40 @@ import { db } from '../firebase';
 import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { WithContext as ReactTags } from 'react-tag-input';
 import * as yup from 'yup'
 import SideNav from './sidenav';
+import '../tags.css';
+
+const KeyCodes = {
+  comma: 188,
+  enter: 13
+};
+
+const delimiters = [KeyCodes.comma, KeyCodes.enter];
 
 function AddProject() {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
   const [githubLink, setGithubLink] = useState('');
   const [projectLink, setProjectLink] = useState('');
-  const userData = collection(db,'projects')
+  const userData = collection(db,'projects');
+  const [tags, setTags] = useState([
+ 
+  ]);
   const navigate = useNavigate();
-  let tagArray = []
+
   const saveData = async () => {
+    const tagsArr = []
+    tags.forEach(tag => {
+      tagsArr.push(tag.text);
+    });
     const x = await addDoc(userData, {
       title: title,
       description: description,
       userGithubLink: githubLink,
       projectGithubLink: projectLink,
-      tags: tagArray
+      tags: tagsArr
     });
     navigate('/viewprojects')
   }
@@ -40,16 +56,30 @@ function AddProject() {
   const onSubmit = (data) =>{
       saveData()
   };
-  const handleChange = (e) => {
-    if(e.target.checked) {
-      tagArray.push(e.target.value)
-    } else {
-      var index = tagArray.indexOf(e.target.value);
-      if (index !== -1) {
-        tagArray.splice(index, 1);
-      }
-    }
-  }
+
+  const suggestions = [
+    { id: "1", text: "Javascript" },
+    { id: "2", text: "Python"},
+    { id: "3", text: "Java" },
+    { id: "4", text: "HTML" },
+    { id: "5", text: "PHP" },
+    { id: "6", text: "TypeScript"},
+    { id: "7", text: "React" },
+    { id: "8", text: "Vue" },
+    { id: "9", text: "Angular" },
+    { id: "10", text: "Bootstrap"},
+    { id: "11", text: "Tailwind" },
+    { id: "12", text: "CSS" },
+];
+
+  const handleAddition = tag => {
+    setTags([...tags, tag]);
+  };
+
+  const handleDelete = i => {
+    setTags(tags.filter((tag, index) => index !== i));
+  };
+
 
   return (
     <section className='flex min-h-screen overflow-y-hidden'>        
@@ -72,79 +102,28 @@ function AddProject() {
               <input type="text" placeholder='Project Github Link'{...register("ProjectGithubLink")} className=' placeholder:text-slate-500 block bg-white w-[90vw]  md:w-[36vw] lg:w-[32vw]  border border-slate-300 rounded-md my-4 py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1' onChange={(e) => setProjectLink(e.target.value)} ></input>
               <p className='text-red-500'>{errors.ProjectGithubLink?.message}</p>
             </div>
-            <div className='md:ml-5 w-11/12 md:w-2/3 lg:w-1/3 my-5 mx-5 md:my-0'>
+            <div className='md:ml-5 w-11/12 h-64 md:w-2/3 lg:w-1/2 my-5 mx-5 md:my-0'>
               <h4 className='mb-2 font-medium'>Select Your Project Tags:</h4>
-              <div className='grid grid-cols-2 md:grid-cols-3 gap-5'>
-                <div className="flex items-center justify-start bg-blue-600 px-1 w-28 rounded-[2px] text-white py-2">
-                  <input id="js-checkbox" type="checkbox" value="Javascript" className="cursor-pointer w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" onChange={e => handleChange(e)}/>
-                  <label htmlFor="js-checkbox" className="ml-1 text-base font-medium">Javascript</label>
-                </div>
 
-                <div className="flex items-center justify-start bg-blue-600 px-1 w-28 rounded-[2px] text-white py-2">
-                  <input id="py-checkbox" type="checkbox" value="Python" className="cursor-pointer w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" onChange={e => handleChange(e)} />
-                  <label htmlFor="py-checkbox" className="ml-1 text-base font-medium">Python</label>
-                </div>
-
-                <div className="flex items-center justify-start bg-blue-600 px-1 w-28 rounded-[2px] text-white py-2">
-                  <input id="java-checkbox" type="checkbox" value="Java" className="cursor-pointer w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" onChange={e => handleChange(e)} />
-                  <label htmlFor="java-checkbox" className="ml-1 text-base font-medium">Java</label>
-                </div>
-
-                <div className="flex items-center justify-start bg-blue-600 px-1 w-28 rounded-[2px] text-white py-2">
-                  <input id="php-checkbox" type="checkbox" value="PHP" className="cursor-pointer w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" onChange={e => handleChange(e)} />
-                  <label htmlFor="php-checkbox" className="ml-1 text-base font-medium">PHP</label>
-                </div>
-
-                <div className="flex items-center justify-start bg-blue-600 px-1 w-28 rounded-[2px] text-white py-2">
-                  <input id="Html-checkbox" type="checkbox" value="Html" className="cursor-pointer w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" onChange={e => handleChange(e)} />
-                  <label htmlFor="Html-checkbox" className="ml-1 text-base font-medium">HTML</label>
-                </div>
-                
-                <div className="flex items-center justify-start bg-blue-600 px-1 w-28 rounded-[2px] text-white py-2">
-                  <input id="Typescript-checkbox" type="checkbox" value="Typescript" className="cursor-pointer w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" onChange={e => handleChange(e)} />
-                  <label htmlFor="Typescript-checkbox" className="ml-1 text-base font-medium">TypeScript</label>
-                </div>
-
-                <div className="flex items-center justify-start bg-blue-600 px-1 w-28 rounded-[2px] text-white py-2">
-                  <input id="ReactJs-checkbox" type="checkbox" value="React" className="cursor-pointer w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" onChange={e => handleChange(e)} />
-                  <label htmlFor="ReactJs-checkbox" className="ml-1 text-base font-medium">React</label>
-                </div>
-                <div className="flex items-center justify-start bg-blue-600 px-1 w-28 rounded-[2px] text-white py-2">
-                  <input id="VueJs-checkbox" type="checkbox" value="Vue" className="cursor-pointer w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" onChange={e => handleChange(e)} />
-                  <label htmlFor="VueJs-checkbox" className="ml-1 text-base font-medium">Vue</label>
-                </div>
-                <div className="flex items-center justify-start bg-blue-600 px-1 w-28 rounded-[2px] text-white py-2">
-                  <input id="Angular-checkbox" type="checkbox" value="Angular" className="cursor-pointer w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" onChange={e => handleChange(e)} />
-                  <label htmlFor="Angular-checkbox" className="ml-1 text-base font-medium">Angular</label>
-                </div>
-                <div className="flex items-center justify-start bg-blue-600 px-1 w-28 rounded-[2px] text-white py-2">
-                  <input id="Bootstrap-checkbox" type="checkbox" value="Bootstrap" className="cursor-pointer w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" onChange={e => handleChange(e)} />
-                  <label htmlFor="Bootstrap-checkbox" className="ml-1 text-base font-medium">Bootstrap</label>
-                </div>
-
-                <div className="flex items-center justify-start bg-blue-600 px-1 w-28 rounded-[2px] text-white py-2">
-                  <input id="Tailwind-checkbox" type="checkbox" value="Tailwind" className="cursor-pointer w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" onChange={e => handleChange(e)} />
-                  <label htmlFor="Tailwind-checkbox" className="ml-1 text-base font-medium">Tailwind</label>
-                </div>
-                
-                <div className="flex items-center justify-start bg-blue-600 px-1 w-28 rounded-[2px] text-white py-2">
-                  <input id="Css-checkbox" type="checkbox" value="Css" className="cursor-pointer w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" onChange={e => handleChange(e)} />
-                  <label htmlFor="Css-checkbox" className="ml-1 text-base font-medium">CSS</label>
-                </div>            
-                
-                <div className="flex items-center justify-start bg-blue-600 px-1 w-28 rounded-[2px] text-white py-2">
-                  <input id="FrontEnd-checkbox" type="checkbox" value="Frontend" className="cursor-pointer w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" onChange={e => handleChange(e)} />
-                  <label htmlFor="FrontEnd-checkbox" className="ml-1 text-base font-medium">Frontend</label>
-                </div>
-                <div className="flex items-center justify-start bg-blue-600 px-1 w-28 rounded-[2px] text-white py-2">
-                  <input id="BackEnd-checkbox" type="checkbox" value="Backend" className="cursor-pointer w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" onChange={e => handleChange(e)} />
-                  <label htmlFor="BackEnd-checkbox" className="ml-1 text-base font-medium">Backend</label>
-                </div>
-                <div className="flex items-center justify-start bg-blue-600 px-1 w-28 rounded-[2px] text-white py-2">
-                  <input id="FullStack-checkbox" type="checkbox" value="Fullstack" className="cursor-pointer w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" onChange={e => handleChange(e)} />
-                  <label htmlFor="FullStack-checkbox" className="ml-1 text-base font-medium">FullStack</label>
-                </div>
-                
+              <div className='w-[350px]'>
+              <ReactTags
+                  tags={tags}
+                  suggestions={suggestions}
+                  delimiters={delimiters}
+                  handleDelete={handleDelete}
+                  handleAddition={handleAddition}
+                  placeholder='Add Skill Tags'
+                  inputFieldPosition="top"
+                  allowDragDrop={false}
+                  classNames={{
+                    tags: 'tagsClass',
+                    tagInput: 'tagInputClass',
+                    tagInputField: 'tagInputFieldClass',
+                    tag: 'tagClass',
+                    suggestions: 'suggestionsClass',
+                    activeSuggestion: 'activeSuggestionClass',
+                  }}
+                />
               </div>
             </div>
           </div>
