d7aba2d8f33129f0e9039da3cd46203b4d6589ec
Lewis ushindi <coderflame3@gmail.com>
2023-06-07 02:40:09 +0300

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,13 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { Disclosure } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import { FaStar } from 'react-icons/fa';
 
 
-function Navbar() {
-  const bgm = "bg-black"
-
+function NavBar() {
   return (
-    <Disclosure as="nav" className="bg-blue-600 z-50 fixed top-0 left-0 right-0">
+    <Disclosure as="nav" className="bg-white text-gray-700 z-50 fixed top-0 left-0 right-0">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -16,7 +15,7 @@ function Navbar() {
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex flex-shrink-0 items-center">
                   <div className="flex justify-center items-center">
-                    <Link to="/" className="animate__animated animate__wobble text-white font-medium">PROJEST</Link>
+                    <Link to="/" className="animate__animated animate__wobble font-medium">PROJEST</Link>
                   </div>
                 </div>
 
@@ -24,58 +23,25 @@ function Navbar() {
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
-                    <Link to="/"><div className="hover:transition-[bg] m-2 md:m-5 hover:bg-opacity-20 hover:backdrop-blur-lg hover:bg-white hover:drop-shadow-lg py-[10px] px-[20px] rounded-[3px] text-white">Home</div></Link>
-                    {/* <Menu as="div" className="relative inline-block text-left">
-                      <div>
-                        <Menu.Button className="hover:transition-[bg] m-2 md:m-5 hover:bg-opacity-20 hover:backdrop-blur-lg hover:bg-white hover:drop-shadow-lg py-[10px] px-[20px] rounded-[3px] text-white">
-                          Projects
-                        </Menu.Button>
-                      </div>
-
-                      <Transition
-                        as={Fragment}
-                        enter="transition ease-out duration-100"
-                        enterFrom="transform opacity-0 scale-95"
-                        enterTo="transform opacity-100 scale-100"
-                        leave="transition ease-in duration-75"
-                        leaveFrom="transform opacity-100 scale-100"
-                        leaveTo="transform opacity-0 scale-95"
-                      >
-                        <Menu.Items className="absolute right-0 z-10 -mt-3 w-56 origin-top-right rounded-md bg-blue-500 text-white focus:outline-none">
-                          <div className="py-1">
-                            <Menu.Item>
-                              {({ active }) => (
-                                <Link to="viewprojects" className={'block px-4 py-2 text-sm hover:bg-opacity-20 hover:backdrop-blur-lg hover:bg-white'}>View Projects</Link>
-                              )}
-                            </Menu.Item>
-                            <Menu.Item>
-                              {({ active }) => (
-                                <Link
-                                  to="addproject"
-                                  className={'block px-4 py-2 text-sm hover:bg-opacity-20 hover:backdrop-blur-lg hover:bg-white'}
-                                >
-                                  Add Project
-                                </Link>
-                              )}
-                            </Menu.Item>
-                          </div>
-                        </Menu.Items>
-                      </Transition>
-                    </Menu>                     */}
-                    <Link to="Registration"><div className="hover:transition-[bg] m-2 md:m-5 hover:bg-opacity-20 hover:backdrop-blur-lg hover:bg-white hover:drop-shadow-lg py-[10px] px-[20px] rounded-[3px] text-white">Register</div></Link>
-                    <Link to="Login"><div className="hover:transition-[bg] m-2 md:m-5 hover:bg-opacity-20 hover:backdrop-blur-lg hover:bg-white hover:drop-shadow-lg py-[10px] px-[20px] rounded-[3px] text-white">Login</div></Link>
-                    <Link to="contributing"><div className="hover:transition-[bg] m-2 md:m-5 hover:bg-opacity-20 hover:backdrop-blur-lg hover:bg-white hover:drop-shadow-lg py-[10px] px-[20px] rounded-[3px] text-white">Contributing</div></Link>
+                    <Link to="/"><div className="hover:transition-[bg] m-2 md:m-5 hover:bg-opacity-20 hover:backdrop-blur-lg  hover:drop-shadow-lg py-[10px] px-[20px] rounded-[3px]">Home</div></Link>                   
+                    <Link to="viewprojects"><div className="hover:transition-[bg] m-2 md:m-5 hover:bg-opacity-20 hover:backdrop-blur-lg hover:drop-shadow-lg py-[10px] px-[20px] rounded-[3px]">Projects</div></Link>
+                    <Link to="Login"><div className="hover:transition-[bg] m-2 md:m-5 hover:bg-opacity-20 hover:backdrop-blur-lg hover:drop-shadow-lg py-[10px] px-[20px] rounded-[3px]">Login</div></Link>
+                    <Link to="contributing"><div className="hover:transition-[bg] m-2 md:m-5 hover:bg-opacity-20 hover:backdrop-blur-lg hover:drop-shadow-lg py-[10px] px-[20px] rounded-[3px]">Contributing</div></Link>
+                    <a href="https://github.com/Frenziecodes/Projest" className="text-yellow-500 hover:text-yellow-300 flex items-center">
+                      <FaStar className="mr-1" />
+                      Star Project
+                    </a>
                   </div>
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-opacity-20 hover:backdrop-blur-lg hover:bg-white hover:drop-shadow-lg hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 hover:bg-opacity-20 hover:backdrop-blur-lg hover:drop-shadow-lg focus:outline-none focus:ring-2 focus:ring-inset">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
-                    <XMarkIcon className="block h-6 w-6 text-white" aria-hidden="true" />
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
                   ) : (
-                    <Bars3Icon className="block h-6 w-6 text-white" aria-hidden="true" />
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
                   )}
                 </Disclosure.Button>
               </div>
@@ -84,46 +50,14 @@ function Navbar() {
 
           <Disclosure.Panel className="sm:hidden">
             <div className="space-y-1 px-2 pt-2 pb-3">
-              <Link to=""><div className="hover:transition-[bg] m-2 md:m-5 hover:bg-opacity-20 hover:backdrop-blur-lg hover:bg-white hover:drop-shadow-lg py-[10px] px-[20px] rounded-[3px] text-white">Home</div></Link>
-              {/* <Menu as="div" className="relative inline-block text-left">
-                <Menu.Button className="hover:transition-[bg] m-2 md:m-5 hover:bg-opacity-20 hover:backdrop-blur-lg hover:bg-white hover:drop-shadow-lg py-[10px] px-[20px] rounded-[3px] text-white w-[90vw]"><div className='text-start'>
-                  Projects
-                </div>
-                </Menu.Button>
-
-                <Transition
-                  as={Fragment}
-                  enter="transition ease-out duration-100"
-                  enterFrom="transform opacity-0 scale-95"
-                  enterTo="transform opacity-100 scale-100"
-                  leave="transition ease-in duration-75"
-                  leaveFrom="transform opacity-100 scale-100"
-                  leaveTo="transform opacity-0 scale-95"
-                >
-                  <Menu.Items className="absolute left-2 z-10 mt-2 w-[90vw] origin-top-right rounded-md bg-blue-500 text-white focus:outline-none">
-                    <div className="py-1">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <Link to="viewprojects" className={'block px-4 py-2 text-sm hover:bg-opacity-20 hover:backdrop-blur-lg hover:bg-white'}>View Projects</Link>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <Link
-                            to="addproject"
-                            className={'block px-4 py-2 text-sm hover:bg-opacity-20 hover:backdrop-blur-lg hover:bg-white'}
-                          >
-                            Add Project
-                          </Link>
-                        )}
-                      </Menu.Item>
-                    </div>
-                  </Menu.Items>
-                </Transition>
-              </Menu> */}
-              <Link to="Registration"><div className="hover:transition-[bg] m-2 md:m-5 hover:bg-opacity-20 hover:backdrop-blur-lg hover:bg-white hover:drop-shadow-lg py-[10px] px-[20px] rounded-[3px] text-white">Register</div></Link>
-              <Link to="Login"><div className="hover:transition-[bg] m-2 md:m-5 hover:bg-opacity-20 hover:backdrop-blur-lg hover:bg-white hover:drop-shadow-lg py-[10px] px-[20px] rounded-[3px] text-white">Login</div></Link>
-              <Link to="contributing"><div className="hover:transition-[bg] m-2 md:m-5 hover:bg-opacity-20 hover:backdrop-blur-lg hover:bg-white hover:drop-shadow-lg py-[10px] px-[20px] rounded-[3px] text-white">Contributing</div></Link>             
+              <Link to="/"><div className="hover:transition-[bg] m-2 md:m-5 hover:bg-opacity-20 hover:backdrop-blur-lg hover:drop-shadow-lg py-[10px] px-[20px] rounded-[3px]">Home</div></Link>             
+              <Link to="viewprojects"><div className="hover:transition-[bg] m-2 md:m-5 hover:bg-opacity-20 hover:backdrop-blur-lg hover:drop-shadow-lg py-[10px] px-[20px] rounded-[3px]">Projects</div></Link>
+              <Link to="Login"><div className="hover:transition-[bg] m-2 md:m-5 hover:bg-opacity-20 hover:backdrop-blur-lg hover:drop-shadow-lg py-[10px] px-[20px] rounded-[3px]">Login</div></Link>
+              <Link to="contributing"><div className="hover:transition-[bg] m-2 md:m-5 hover:bg-opacity-20 hover:backdrop-blur-lg hover:drop-shadow-lg py-[10px] px-[20px] rounded-[3px]">Contributing</div></Link>
+              <a href="https://github.com/Frenziecodes/Projest" className="text-yellow-500 hover:text-yellow-300 flex items-center">
+                <FaStar className="mr-1" />
+                Star Project
+              </a>           
             </div>
           </Disclosure.Panel>
         </>
@@ -132,4 +66,4 @@ function Navbar() {
   );
 }
 
-export default Navbar;
+export default NavBar;
--- src/pages/AddProject.jsx
+++ src/pages/AddProject.jsx
@@ -1,12 +1,12 @@
-import React, { useState } from 'react'
-import { collection, addDoc } from "firebase/firestore"; 
+import React, { useState } from 'react';
+import { collection, addDoc } from "firebase/firestore";
 import { db } from '../firebase';
 import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { WithContext as ReactTags } from 'react-tag-input';
-import * as yup from 'yup'
-import SideNav from './sidenav';
+import * as yup from 'yup';
+import NavBar from '../components/Navbar';
 import '../tags.css';
 
 const KeyCodes = {
@@ -21,56 +21,55 @@ function AddProject() {
   const [description, setDescription] = useState('');
   const [githubLink, setGithubLink] = useState('');
   const [projectLink, setProjectLink] = useState('');
-  const userData = collection(db,'projects');
-  const [tags, setTags] = useState([
- 
-  ]);
+  const [demoLink, setDemoLink] = useState('');
+  const userData = collection(db, 'projects');
+  const [tags, setTags] = useState([]);
   const navigate = useNavigate();
 
   const saveData = async () => {
-    const tagsArr = []
-    tags.forEach(tag => {
-      tagsArr.push(tag.text);
-    });
-    const x = await addDoc(userData, {
+    const tagsArr = tags.map(tag => tag.text);
+    const projectData = {
       title: title,
       description: description,
       userGithubLink: githubLink,
       projectGithubLink: projectLink,
+      demoLink: demoLink,
       tags: tagsArr
-    });
-    navigate('/viewprojects')
-  }
-  const schema =yup.object({
-      title:yup.string().required("Project Title is required"),
-      Description:yup.string().required("Project Description is required"),
-      UserGithubLink:yup.string().required("User GithubLink is required"),
-      ProjectGithubLink:yup.string().required("Project GithubLink is required"),
+    };
+    await addDoc(userData, projectData);
+    navigate('/viewprojects');
+  };
 
-
+  const schema = yup.object({
+    title: yup.string().required("Project Title is required"),
+    description: yup.string().required("Project Description is required"),
+    userGithubLink: yup.string().required("User Github Link is required"),
+    projectGithubLink: yup.string().required("Project Github Link is required"),
+    demoLink: yup.string().required("Demo Link is required"),
   }).required();
-  
-  const { register, handleSubmit, formState:{ errors } } = useForm({
+
+  const { register, handleSubmit, formState: { errors } } = useForm({
     resolver: yupResolver(schema)
   });
-  const onSubmit = (data) =>{
-      saveData()
+
+  const onSubmit = (data) => {
+    saveData();
   };
 
   const suggestions = [
     { id: "1", text: "Javascript" },
-    { id: "2", text: "Python"},
+    { id: "2", text: "Python" },
     { id: "3", text: "Java" },
     { id: "4", text: "HTML" },
     { id: "5", text: "PHP" },
-    { id: "6", text: "TypeScript"},
+    { id: "6", text: "TypeScript" },
     { id: "7", text: "React" },
     { id: "8", text: "Vue" },
     { id: "9", text: "Angular" },
-    { id: "10", text: "Bootstrap"},
+    { id: "10", text: "Bootstrap" },
     { id: "11", text: "Tailwind" },
     { id: "12", text: "CSS" },
-];
+  ];
 
   const handleAddition = tag => {
     setTags([...tags, tag]);
@@ -80,33 +79,29 @@ function AddProject() {
     setTags(tags.filter((tag, index) => index !== i));
   };
 
-
   return (
-    <section className='flex min-h-screen overflow-y-hidden'>        
-        <SideNav />       
-
-        <div className='flex md:pl-10 flex-col items-center ml-1 md:ml-60 h-[100%] pb-[50px] md:pb-[115px] pt-10 Context'>
-          <form onSubmit={handleSubmit(onSubmit)}>
-          <h2 className="font-medium leading-tight text-2xl mt-10 md:mt-[50px] mb-4 text-blue-600">Add your project details 👇</h2>
-          <div className='border-b-[3px] border-black mb-10 w-[200px]'></div>
+    <section className=''>
+      <NavBar/>
+      <div className='flex md:pl-10 flex-col items-center ml-1 md:ml-60 h-[100%] pb-4 mb-8 pt-10 Context'>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <h2 className="font-medium leading-tight text-md mt-10 md:mt-[50px] mb-4 text-gray-700">Add your project details by filling the form below</h2>          
           <div className='flex flex-col md:flex-row justify-start items-start w-[100%]'>
             <div className='flex flex-col items-center md:items-end justify-center w-[100%] md:w-[40%] lg:w-[50%]'>
-              <input type="text" placeholder='Project Title' {...register("title")} className=' placeholder:text-slate-500 block bg-white w-[90vw] md:w-[36vw] lg:w-[32vw] border border-slate-300 rounded-md my-4 py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1' onChange={(e) => setTitle(e.target.value)} ></input>
-              <p className='text-red-500'>{errors.title?.message}</p>
-              <textarea placeholder='Project Description'{...register("Description")}  className=' placeholder:text-slate-500 block bg-white w-[90vw]  md:w-[36vw] lg:w-[32vw]  border border-slate-300 rounded-md my-4 py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 ' onChange={(e) => setDescription(e.target.value)}  ></textarea>
-              <p className='text-red-500'>{errors.Description?.message}</p>
-
-              <input type="text" placeholder='User Github Link'{...register("UserGithubLink")} className=' placeholder:text-slate-500 block bg-white w-[90vw]  md:w-[36vw] lg:w-[32vw]  border border-slate-300 rounded-md my-4 py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 ' onChange={(e) => setGithubLink(e.target.value)} ></input>
-              <p className='text-red-500'>{errors.UserGithubLink?.message}</p>
-
-              <input type="text" placeholder='Project Github Link'{...register("ProjectGithubLink")} className=' placeholder:text-slate-500 block bg-white w-[90vw]  md:w-[36vw] lg:w-[32vw]  border border-slate-300 rounded-md my-4 py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1' onChange={(e) => setProjectLink(e.target.value)} ></input>
-              <p className='text-red-500'>{errors.ProjectGithubLink?.message}</p>
+              <input type="text" placeholder='Project Title' {...register("title")} className='placeholder:text-slate-500 block bg-white w-[90vw] md:w-[36vw] lg:w-[32vw] border border-slate-300 rounded-md my-4 py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1' onChange={(e) => setTitle(e.target.value)}></input>
+              <p className='text-red-500'>{errors.title?.message}</p> 
+              <textarea placeholder='Project Description' {...register("description")} className='placeholder:text-slate-500 block bg-white min-h-[170px] w-[90vw] md:w-[36vw] lg:w-[32vw] border border-slate-300 rounded-md my-4 py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1' onChange={(e) => setDescription(e.target.value)}></textarea>
+              <p className='text-red-500'>{errors.description?.message}</p>                            
             </div>
-            <div className='md:ml-5 w-11/12 h-64 md:w-2/3 lg:w-1/2 my-5 mx-5 md:my-0'>
-              <h4 className='mb-2 font-medium'>Select Your Project Tags:</h4>
 
-              <div className='w-[350px]'>
-              <ReactTags
+            <div className='md:ml-5 w-11/12 h-64 md:w-2/3 lg:w-1/2 my-5 mx-5 md:my-0'>
+              <input type="text" placeholder='User Github Link' {...register("userGithubLink")} className='placeholder:text-slate-500 block bg-white w-[90vw] md:w-[36vw] lg:w-[32vw] border border-slate-300 rounded-md my-4 py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1' onChange={(e) => setGithubLink(e.target.value)}></input>
+              <p className='text-red-500'>{errors.userGithubLink?.message}</p>
+              <input type="text" placeholder='Project Github Link' {...register("projectGithubLink")} className='placeholder:text-slate-500 block bg-white w-[90vw] md:w-[36vw] lg:w-[32vw] border border-slate-300 rounded-md my-4 py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1' onChange={(e) => setProjectLink(e.target.value)}></input>
+              <p className='text-red-500'>{errors.projectGithubLink?.message}</p>
+              <input type="text" placeholder='Demo Link' {...register("demoLink")} className='placeholder:text-slate-500 block bg-white w-[90vw] md:w-[36vw] lg:w-[32vw] border border-slate-300 rounded-md my-4 py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1' onChange={(e) => setDemoLink(e.target.value)}></input>
+              <p className='text-red-500'>{errors.demoLink?.message}</p>
+              <div className='placeholder:text-slate-500 block bg-white w-[90vw] md:w-[36vw] lg:w-[32vw] border border-slate-300 rounded-md my-4 py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1'>
+                <ReactTags
                   tags={tags}
                   suggestions={suggestions}
                   delimiters={delimiters}
@@ -127,14 +122,13 @@ function AddProject() {
               </div>
             </div>
           </div>
-          <button className="px-6 py-2 mt-8 font-medium text-white bg-blue-500 rounded-md transition-all duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"  type='submit' >
+          <button className="px-6 py-2 mt-8 font-medium text-white bg-blue-500 rounded-md transition-all duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50" type='submit'>
             Save Project
           </button>
-          </form>
-        </div>
+        </form>
+      </div>
     </section>
   )
 }
 
-export default AddProject
-// onClick={saveData}
+export default AddProject;
