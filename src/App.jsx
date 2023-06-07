5229a4807376a76a8a4c2c4625c1831ae400ace3
Lewis ushindi <coderflame3@gmail.com>
2023-06-07 06:21:50 +0300

    redirecting users to 404 page on incorrect url

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,43 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import AddProject from './pages/AddProject'
-import DocsPage from './pages/DocsPage'
-import HomePage from './pages/HomePage'
-import ViewProject from './pages/ViewProject'
-import Contributing from './pages/Contributing'
-import Registration from './pages/Registration'
-import Login from './pages/Login'
-import Dashboard from './pages/Dashboard'
-import MyProjects from './pages/MyProjects'
-import Notifications from './pages/Notifications'
-import Profile from './pages/Profile'
-import Logout from './pages/Logout'
-import Layout from './components/Layout'
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AddProject from './pages/AddProject';
+import DocsPage from './pages/DocsPage';
+import HomePage from './pages/HomePage';
+import ViewProject from './pages/ViewProject';
+import Contributing from './pages/Contributing';
+import Registration from './pages/Registration';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import MyProjects from './pages/MyProjects';
+import Notifications from './pages/Notifications';
+import Profile from './pages/Profile';
+import Logout from './pages/Logout';
+import Layout from './components/Layout';
+import NotFound from './pages/404';
 
 function App() {
   return (
     <BrowserRouter>
       <Layout>
         <Routes>
+          <Route path='/404' element={<NotFound />} />
           <Route path='/' element={<HomePage />} />
           <Route path='/docs' element={<DocsPage />} />
           <Route path='/viewprojects' element={<ViewProject />} />
           <Route path='/addproject' element={<AddProject />} />
           <Route path='/contributing' element={<Contributing />} />
-          <Route path='/Registration' element={<Registration />} />
-          <Route path='/Login' element={<Login />} />
+          <Route path='/registration' element={<Registration />} />
+          <Route path='/login' element={<Login />} />
           <Route path='/dashboard' element={<Dashboard />} />
           <Route path='/myprojects' element={<MyProjects />} />
           <Route path='/notifications' element={<Notifications />} />
           <Route path='/profile' element={<Profile />} />
           <Route path='/logout' element={<Logout />} />
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </Layout>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/404.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function NotFound() {
+  return (
+    <div className="flex flex-col bg-white text-gray-700 items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold mb-4">404 Page Not Found</h1>
+      <p className="text-gray-600">Oops! The page you are looking for does not exist.</p>
+      <p className="text-gray-600">Please check the URL or go back to the homepage.</p>     
+      
+    </div>
+  );
+}
+
+export default NotFound;
