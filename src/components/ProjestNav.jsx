f591f598770a2e409b98b4a3b669e7053fa410b0
Lewis ushindi <coderflame3@gmail.com>
2023-06-07 13:53:23 +0300

    updating nav

--- src/components/ProjestNav.jsx
+++ src/components/ProjestNav.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Disclosure } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
-import { FaStar } from 'react-icons/fa';
-
 
 function ProjestNav() {
   return (
@@ -27,9 +25,10 @@ function ProjestNav() {
                     <Link to="viewprojects"><div className="hover:transition-[bg] m-2 md:m-5 hover:bg-opacity-20 hover:backdrop-blur-lg hover:drop-shadow-lg py-[10px] px-[20px] rounded-[3px]">Projects</div></Link>   
                     <Link to="/addproject"><div className="hover:transition-[bg] m-2 md:m-5 hover:bg-opacity-20 hover:backdrop-blur-lg hover:drop-shadow-lg py-[10px] px-[20px] rounded-[3px]">Add Project</div></Link>                 
                     {/* <Link to="contributing"><div className="hover:transition-[bg] m-2 md:m-5 hover:bg-opacity-20 hover:backdrop-blur-lg hover:drop-shadow-lg py-[10px] px-[20px] rounded-[3px]">Contributing</div></Link> */}
-                    <a href="https://github.com/Frenziecodes/Projest" className="text-yellow-500 hover:text-yellow-300 flex items-center">
-                      <FaStar className="mr-1" />
-                      Star Project
+                    <a href="https://github.com/Frenziecodes/Projest" className="flex items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32.58 31.77" height="30">
+                      <path d="M16.29,0A16.29,16.29,0,0,0,.42,17.54c0,7.29,4.72,13.49,11.28,15.67.82.15,1.11-.36,1.11-.79,0-.39,0-1.42,0-2.79-4.59,1-5.56-2.21-5.56-2.21A4.39,4.39,0,0,0,4.2,25.17c-1.51-.84.11-.82.11-.82a3.47,3.47,0,0,1,2.53,1.71,3.48,3.48,0,0,0,4.77,1.36,3.49,3.49,0,0,1,1-2.19c-3.6-.41-7.4-1.8-7.4-8a6.28,6.28,0,0,1,1.68-4.34,5.81,5.81,0,0,1,.16-4.29s1.36-.43,4.45,1.66a15.3,15.3,0,0,1,8.12,0C22.77,6.43,24.13,7.86,24.13,7.86a5.8,5.8,0,0,1,.16,4.29,6.27,6.27,0,0,1,1.68,4.34c0,6.23-3.81,7.58-7.44,7.98a3.9,3.9,0,0,1,1.12,3,.79.79,0,0,0,.09.32c0,.38.09,1.4,0,2.79,0,.43.29.94,1.12.79A16.29,16.29,0,0,0,16.29,0Z" />
+                    </svg>
                     </a>
                   </div>
                 </div>
@@ -53,9 +52,9 @@ function ProjestNav() {
               <Link to="/"><div className="hover:transition-[bg] m-2 md:m-5 hover:bg-opacity-20 hover:backdrop-blur-lg hover:drop-shadow-lg py-[10px] px-[20px] rounded-[3px]">Home</div></Link>             
               <Link to="viewprojects"><div className="hover:transition-[bg] m-2 md:m-5 hover:bg-opacity-20 hover:backdrop-blur-lg hover:drop-shadow-lg py-[10px] px-[20px] rounded-[3px]">Projects</div></Link>  
               <Link to="/addproject"><div className="hover:transition-[bg] m-2 md:m-5 hover:bg-opacity-20 hover:backdrop-blur-lg hover:drop-shadow-lg py-[10px] px-[20px] rounded-[3px]">Add Project</div></Link> 
-              <a href="https://github.com/Frenziecodes/Projest" className="m-2 flex items-center py-[10px] px-[20px] rounded-[3px]">
+              {/* <a href="https://github.com/Frenziecodes/Projest" className="m-2 flex items-center py-[10px] px-[20px] rounded-[3px]">
                 Star Project
-              </a>           
+              </a>            */}
             </div>
           </Disclosure.Panel>
         </>
