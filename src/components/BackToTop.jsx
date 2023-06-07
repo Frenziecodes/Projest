3167a645de5ee74b76dc89875933e2af778e83cb
Lewis ushindi <coderflame3@gmail.com>
2023-06-07 11:38:53 +0300

    fixing back to top button

--- src/components/BackToTop.jsx
+++ src/components/BackToTop.jsx
@@ -17,7 +17,22 @@ function BackToTop(){
         window.scrollTo(0, 0);
     }
     return (
-        <button onClick={pushUserToTop} type="button" className={`rounded ${hideBackToTop ? "hidden" : ""} fixed bg-slate-900 hover:bg-slate-800 hover:scale-105 duration-300 text-white p-5 bottom-10 right-5`}>Back to top</button>
+        <button onClick={pushUserToTop} type="button" className={`rounded ${hideBackToTop ? "hidden" : ""} fixed bg-blue-900 hover:bg-blue-800 hover:scale-105 duration-300 rounded-full text-white p-3 bottom-10 right-5`}>
+             <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                stroke-width="2.5"
+                stroke="currentColor"
+                class="h-4 w-4">
+                <path
+                fill-rule="evenodd"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 19.5v-15m0 0l-6.75 6.75M12 4.5l6.75 6.75"
+                clip-rule="evenodd" />
+            </svg>
+        </button>
     )
 }
 
