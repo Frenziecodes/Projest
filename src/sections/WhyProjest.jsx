94482d6f10150589255dc92e6b42e9c5c1718bde
Lewis ushindi <coderflame3@gmail.com>
2023-06-07 10:57:56 +0300

    updating color scheme

--- src/sections/WhyProjest.jsx
+++ src/sections/WhyProjest.jsx
@@ -24,12 +24,12 @@ const WhyProjestSection = () => {
             <div className="flex flex-col md:flex-row items-center">
               <div className="w-full md:w-1/2">
                 <div className="px-6 py-12 md:px-12">
-                  <h2 className="mb-6 text-xl lg:text-3xl font-bold">Submit Your Projects with Ease</h2>
+                  <h2 className="mb-6 text-xl lg:text-3xl font-bold text-white">Submit Your Projects with Ease</h2>
                   <p className="mb-6 pb-2 text-white">
                     Showcase your projects by filling out our user-friendly submission form.
                   </p>
                   <div className="">
-                    <Link to="addproject" className="py-2 px-6 rounded-md bg-white text-blue-900 font-semibold shadow-lg transition duration-300 ease-in-out hover:bg-blue-400 hover:text-white">
+                    <Link to="addproject" className="py-2 px-6 rounded-md bg-white text-blue-900 font-semibold shadow-lg transition duration-300 ease-in-out hover:bg-blue-800 hover:text-white">
                       Add Project
                     </Link>
                   </div>
@@ -46,7 +46,7 @@ const WhyProjestSection = () => {
           </div>
         </section>
 
-        <section className="my-6 mx-6 text-center md:text-left bg-gradient-to-r from-blue-900 to-violet-500 rounded-md">
+        <section className="my-6 mx-6 text-center md:text-left bg-gradient-to-l from-blue-900 to-violet-500 rounded-md">
           <div className="block rounded-lg">
             <div className="flex flex-col md:flex-row items-center">
               <div className="flex justify-center items-center w-full md:w-1/2 p-6">
@@ -58,12 +58,12 @@ const WhyProjestSection = () => {
               </div>
               <div className="w-full md:w-1/2">
                 <div className="px-6 py-12 md:px-12">
-                  <h2 className="mb-6 text-xl lg:text-3xl font-bold md:text-right">Explore Exciting Projects</h2>
+                  <h2 className="mb-6 text-xl lg:text-3xl font-bold md:text-right text-white">Explore Exciting Projects</h2>
                   <p className="mb-6 pb-2 text-white md:text-right">
                     Discover a wide range of projects submitted by talented individuals.
                   </p>
                   <div className="flex justify-center md:justify-end">
-                    <Link to="viewprojects" className="py-2 px-6 rounded-md bg-white text-blue-900 font-semibold shadow-lg transition duration-300 ease-in-out hover:bg-blue-400 hover:text-white">
+                    <Link to="viewprojects" className="py-2 px-6 rounded-md bg-white text-blue-900 font-semibold shadow-lg transition duration-300 ease-in-out hover:bg-blue-800 hover:text-white">
                       Explore Projects
                     </Link>
                   </div>
