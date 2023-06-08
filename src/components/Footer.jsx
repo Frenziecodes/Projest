82a5f8ed631e71dcd81bd03905bcdbb69a18bae1
Lewis ushindi <coderflame3@gmail.com>
2023-06-08 12:05:09 +0300

    updating

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import { FaStar, FaCodeBranch } from 'react-icons/fa';
+import LinksComponent from './LinksComponent';
 
 function Footer() {
+  const solutions = [
+    { url: '#!', text: 'Explore projects' },
+    { url: '#!', text: 'Add Project' },
+    { url: '#!', text: 'Contribution' },
+  ]
+  const support = [
+    { url: '#!', text: 'Report Bug' },
+    { url: '#!', text: 'Request Feature' },
+    { url: '#!', text: 'Documentation' },
+  ]
   return (
-    <footer className="bg-white text-gray-700 py-6">
+    <footer className="bg-blue-800 text-white py-6">
       <div className="container mx-auto flex flex-col justify-between items-center">
         <div className="text-center mb-4">
           <h2 className="text-3xl mb-3 font-bold">Projest</h2>
-          <p className="text-sm px-2 md:px-0">A platform for showcasing and sharing projects. For any issues or feature requests, feel free to open an issue on our GitHub repository.</p>
+          <p className="text-sm px-2 md:px-0">A platform for showcasing and sharing projects. If you find this platform helpful, we would appreciate it if you could <a href="https://github.com/Frenziecodes/Projest" target="_blank" rel="noopener noreferrer">star</a> the project on <a href="https://github.com/Frenziecodes/Projest" target="_blank" rel="noopener noreferrer">GitHub</a>.</p>
         </div>
-        <div className="flex items-center space-x-4">
-          <a href="https://github.com/Frenziecodes/Projest" className="hover:text-yellow-700 flex items-center">
-            <FaStar className="mr-1" />
-            Star Project
-          </a>
-          <a href="https://github.com/Frenziecodes/Projest/fork" className="flex items-center hover:text-blue-700">
-            <FaCodeBranch className="mr-1" />
-            Fork Project
-          </a>
-        </div>
+      </div>
+      <div className="grid md:grid-cols-2 lg:grid-cols-2 place-items-center my-2">
+        <LinksComponent label="Solutions" links={solutions} />
+        <LinksComponent label="Support" links={support} />        
       </div>
       <div className="text-center mt-4">
         © {new Date().getFullYear()} All rights reserved.
---
+++ src/components/LinksComponent.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const LinksComponent = ({ label, links }) => {
+  return (
+    <div className="mb-6">
+      <h5 className="mb-2.5 font-bold uppercase text-white">
+        {label}
+      </h5>
+
+      <ul className="mb-0 list-none">
+        {links.map((link, index) => (
+          <li key={index}>
+            <a href={link.url} className="text-neutral-900 text-sm">
+              {link.text}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default LinksComponent;
