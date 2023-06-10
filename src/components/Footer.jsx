75b19e11b2e92f51328248864be8599630fe550a
Lewis ushindi <coderflame3@gmail.com>
2023-06-10 16:42:19 +0300

    updating footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,6 +18,11 @@ function Footer() {
     { url: '/documentation', text: 'Documentation' },
     { url: '#!', text: 'Developer Resources' },
   ]
+  const legal = [
+    { url: '#!', text: 'License' },
+    { url: '#!', text: 'Privacy Policy' },
+    { url: '#!', text: 'Terms of service' },
+  ]
   return (
     <footer className="bg-blue-800 text-white py-6">
       <div className="container mx-auto flex flex-col justify-between items-center">
@@ -26,10 +31,11 @@ function Footer() {
           <p className="text-sm px-2 md:px-0">A platform for showcasing and sharing projects. If you find this platform helpful, we would appreciate it if you could <a href="https://github.com/Frenziecodes/Projest" target="_blank" rel="noopener noreferrer">star</a> the project on <a href="https://github.com/Frenziecodes/Projest" target="_blank" rel="noopener noreferrer">GitHub</a>.</p>
         </div>
       </div>
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 place-items-center my-2">
+      <div className="grid md:grid-cols-2 lg:grid-cols-4 place-items-center my-2">
         <LinksComponent label="Solutions" links={getInVolved} />
         <LinksComponent label="Support" links={support} />        
         <LinksComponent label="resources" links={resources} />        
+        <LinksComponent label="Legal" links={legal} />        
       </div>
       <div className="text-center mt-4">
         © {new Date().getFullYear()} All rights reserved.
