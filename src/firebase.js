0c36a5fd2f74d102ff2797a208888a11a4055608
Lewis ushindi <coderflame3@gmail.com>
2023-06-11 22:13:56 +0300

    update

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import { getFirestore } from "@firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
-};
+  apiKey: "AIzaSyCxXu5d2dB_QMe8JEdJq0HjJkrpvsoXQBA",
+  authDomain: "projest-lewyy.firebaseapp.com",
+  projectId: "projest-lewyy",
+  storageBucket: "projest-lewyy.appspot.com",
+  messagingSenderId: "86960042542",
+  appId: "1:86960042542:web:08a20a9fdae740f728e5bf",
+  measurementId: "G-QDG3V3P7S0"
+}
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
